3dc78a9e92fef02a5bf8c5dd3a599e38d8fc7425
Luan Santos <cfcluan@gmail.com>
2012-02-21 15:13:25 -0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberFacebook::Application.config.secret_token = '60e633f7889930600847206b25389e17710cb60f8583a584b0cfbcae4048fe5018628e152f9f312ff15d7859f04243117c5dadd5e9ec09eeae5166a2415d2bd7'
